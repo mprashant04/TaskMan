18ae59c5a4ee4e92abd66af65ab52174ee923c53
pm <pman.social04@gmail.com>
2023-04-07 19:57:08 +0530

    dddd

--- app/src/main/java/com/example/taskman/common/Declarations.java
+++ app/src/main/java/com/example/taskman/common/Declarations.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 public class Declarations {
 
-    public static final String APK_VERSION = "0.81";
+    public static final String APK_VERSION = "0.82";
 
     public static final int MAX_TASK_NOTIFICATIONS = 10;
 
